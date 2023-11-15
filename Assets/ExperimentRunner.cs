30010b891ed14425867e52051db1da632aa8a602
jiamingxu1 <jxu493@gmail.com>
2023-11-14 22:20:28 -0600

    Modified ExperimentRunner, finished code structure

--- Assets/ExperimentRunner.cs
+++ Assets/ExperimentRunner.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class ExperimentRunner : MonoBehaviour
 {
+    
+    // Define some objects and variables 
+    public GameObject fixationCross; //CREATE IN UNITY
+    public Text responsePrompt; // CREATE IN Unity UI
+
     private void Init()
     {
         Debug.Log("Init");
@@ -38,6 +43,8 @@ public class ExperimentRunner : MonoBehaviour
 
     private void StartConditions()
     {
+        fixationCross.SetActive(true); //present fixation cross
+
         if(Input.GetKeyDown(KeyCode.Space))
         {
             playerReady = true;
@@ -49,7 +56,7 @@ public class ExperimentRunner : MonoBehaviour
             return;
         }
        
-        if(playerFixating < 1f) //player has to fixate for 1 s to start the trial 
+        if(playerFixating < 1f) //player has to fixate for 1s to start the trial 
         {
             playerFixating += Time.deltaTime; 
             return;
@@ -59,7 +66,8 @@ public class ExperimentRunner : MonoBehaviour
         CameraBias.SetBias(currentTrialType,currentTrialSign,currentTrialIncrement); //?
         //CameraBias.NullCameraBias(false);
         DataWriter.Open();
-        trialState = "Trial"; //advancing to the next state, start the trial
+        //advancing to the next state, start the trial
+        trialState = "Trial"; 
         Debug.Log("Trial #" + currentTrial.ToString() + ": " + currentTrialType + "_" + currentTrialSign + "_" + currentTrialIncrement + ", " + currentTrialEnvironment);
     }
 
@@ -89,32 +97,127 @@ public class ExperimentRunner : MonoBehaviour
         yield return new WaitForSeconds(0.2f); // Wait for 200 ms
         trialImages[2].SetActive(false);
 
+         // Hide fixation cross
+        fixationCross.SetActive(false); 
+
+        yield return new WaitForSeconds(0.15f); // Wait for 150 ms inter-trial-interval
+
         // After showing all images, proceed to get the response
         PromptForResponse();
-        
-        // Record response
-        DataWriter.WriteData(); 
     }
 
     private void PromptForResponse()
     {
-        // Code to prompt the user for a response
+        responsePrompt.text = "Please enter your response (1 or 2)";
+        responsePrompt.gameObject.SetActive(true);
+    }
+   
+    private void ProcessResponse(int response)
+    {
+    // Deactivate the prompt text
+    responsePrompt.gameObject.SetActive(false);
+
+    // Record response
+    DataWriter.WriteData(response); 
+    
+    // Handle the response
+    Debug.Log("User responded with: " + response);
     }
 
-    
-    
-    
-    
-
-    // Start is called before the first frame update
-    void Start()
+    private void EndConditon()
     {
-        
+        // check if response is properly saved 
+        bool responseSaved = DataWriter.CheckIfRespSaved(userResponse) //MODIFY DATAWRITER
+        if (responseSaved)
+        {
+            Debug.Log("Trial ended, response saved");
+            userResponse = 0; //reset user response??
+            DataWriter.Close();
+            trialState = "PostTrial";
+        }
+        else
+        {
+            Debug.LogError("Error: response not saved.");
+        }
+    }
+
+    private void PostTrial()
+    {
+        currentTrial++;
+        trialState = "PreTrial";
+    }
+    
+    private void Exit()
+    {
+        isExperiment = false;
+    }
+    
+    private void UpdateTrialDetails()  //CHANGE THIS
+    {
+        currentTrialType = TrialGenerator.trialList[currentTrial].Split("_")[0];
+        currentTrialSign = TrialGenerator.trialList[currentTrial].Split("_")[1];
+        currentTrialIncrement = TrialGenerator.trialList[currentTrial].Split("_")[2];
+        currentTrialEnvironment = TrialGenerator.trialList[currentTrial].Split("_")[3];
+        //currentTrialEnvironment = "FullFlow";
+    }
+
+    private void Awake()
+    {
+        currentTrialType = "";
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(Input.GetKeyDown(KeyCode.E)) Init();
         
+        // HERE do eye tracker calibration
+        
+        if(!isExperiment) return;
+
+        if(TrialGenerator.trialList == null) //CHANGE THIS
+        {
+            isExperiment = false;
+            Debug.Log("Init failed.");
+            return;
+        }
+
+        //Check for response
+        if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            ProcessResponse(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+        {
+            ProcessResponse(2);
+        }
+        
+        //End of the experiment, all trials done
+        if(currentTrial == TrialGenerator.trialList.Count)
+        {
+            Exit();
+            return;
+        }
+
+        switch(trialState)
+        {
+            case "PreTrial":
+                PreTrial();
+                break;
+            case "StartCheck":
+                StartConditions();
+                break;
+            case "Trial":
+                Trial();
+                EndConditions();
+                break;
+            case "PostTrial":
+                PostTrial();
+                break;
+            default:
+                Debug.Log("ERROR: Missing/Incorrect Trial State.");
+                Time.timeScale = 0;
+                break;
+        }
     }
 }
